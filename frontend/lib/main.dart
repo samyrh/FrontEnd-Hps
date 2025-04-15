ac0242f7d361ed2fb78cc849ab2c5c996b8f99bc
Samy <Rhalimsami8@gmail.com>
2025-04-16 00:03:45 +0100

    last update for today

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const Changepassword(),
+      home: const AddNewCard(),
     );
   }
 }
