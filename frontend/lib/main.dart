9403dbf1b2405bf987a7eacf378fcda6209c04b8
Samy <Rhalimsami8@gmail.com>
2025-05-08 09:33:43 +0100

    Commit for add App Route in main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,15 +3,18 @@ import 'package:sami/screens/AiScreen1.dart';
 import 'package:sami/screens/AiScreen2.dart';
 import 'package:sami/screens/AiScreen3.dart';
 import 'package:sami/screens/BrainBoxScreen.dart';
+import 'package:sami/screens/CardPacksScreen.dart';
 import 'package:sami/screens/ChangeLanguagesScreen.dart';
 import 'package:sami/screens/ChangePassword.dart';
 import 'package:sami/screens/ChooseCardColorScreen.dart';
 import 'package:sami/screens/ComplainScreen.dart';
+import 'package:sami/screens/ContactUs.dart';
 import 'package:sami/screens/Home.dart';
 import 'package:sami/screens/Landing1.dart';
 import 'package:sami/screens/Landing2.dart';
 import 'package:sami/screens/Landing3.dart';
 import 'package:sami/screens/Landing4.dart';
+import 'package:sami/screens/Menu.dart';
 import 'package:sami/screens/NewCard.dart';
 import 'package:sami/screens/NotificationParametre.dart';
 import 'package:sami/screens/NotificationsScreen.dart';
@@ -42,12 +45,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Test Screens Slider',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomeScreen(), // ✅ Correct casing
+      home: const SignInScreen(), // ✅ Correct casing
     );
   }
 }
