ef4d9c108fb4fc8ea60c7d3b235062868b0cc7a8
Samy <Rhalimsami8@gmail.com>
2025-05-15 12:49:14 +0100

    Commit for the set up of microservice User (includes cradholder + agent + Oauth2 + jwt

---
+++ microservices/user-service/src/main/java/hps/ma/userservice/dao/entities/Agent.java
@@ -0,0 +1,37 @@
+package hps.ma.userservice.dao.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor@NoArgsConstructor
+public class Agent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @Column(name = "is_admin", nullable = false)
+    private boolean isAdmin;
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+    @OneToMany(mappedBy = "senderAgent", cascade = CascadeType.ALL)
+    private List<Event> events;
+
+    @OneToMany(mappedBy = "approver", cascade = CascadeType.ALL)
+    private List<TravelPlan> approvedPlans;
+
+    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL)
+    private List<Card> managedCards;
+}
---
+++ microservices/user-service/src/main/java/hps/ma/userservice/dao/entities/Cardholder.java
@@ -0,0 +1,47 @@
+package hps.ma.userservice.dao.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+
+@Entity
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cardholder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 150, nullable = false)
+    private String password;
+
+    @Column(name = "is_locked", nullable = false)
+    private boolean isLocked;
+
+    @Column(name = "login_attempts", nullable = false)
+    private int loginAttempts = 0;
+
+    @Column(name = "biometric_enabled", nullable = false)
+    private boolean biometricEnabled;
+
+    @Column(length = 6)
+    private String securityCode;
+
+    @OneToMany(mappedBy = "cardholder", cascade = CascadeType.ALL)
+    private List<Card> cards;
+
+    @OneToMany(mappedBy = "recipient", cascade = CascadeType.ALL)
+    private List<Event> events;
+}
