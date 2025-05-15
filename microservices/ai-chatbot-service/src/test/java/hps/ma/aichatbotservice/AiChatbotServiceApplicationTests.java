1bf8bfbfa6919c4bf11d76d7dac46b9a5363aa7d
Samy <Rhalimsami8@gmail.com>
2025-05-15 11:46:49 +0100

    Commit for set up of service chatbot

---
+++ microservices/ai-chatbot-service/src/test/java/hps/ma/aichatbotservice/AiChatbotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hps.ma.aichatbotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatbotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
