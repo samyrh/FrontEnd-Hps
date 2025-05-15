68295c366d09d09557a0f309af1532474a629cfb
Samy <Rhalimsami8@gmail.com>
2025-05-15 11:57:26 +0100

    Commit for the set up of microservice Travel Plan

---
+++ microservices/travel-plan-service/src/main/java/hps/ma/travelplanservice/TravelPlanServiceApplication.java
@@ -0,0 +1,13 @@
+package hps.ma.travelplanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlanServiceApplication.class, args);
+    }
+
+}
---
+++ microservices/travel-plan-service/src/test/java/hps/ma/travelplanservice/TravelPlanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hps.ma.travelplanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
