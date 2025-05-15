0971eb3d051a38d45e7f9525f3c166f74bb6e294
NADA <nadasenhaji@gmail.com>
2025-05-15 15:50:11 +0100

    commit for add lay

---
+++ microservices/transactions-service/src/main/java/hps/ma/transactionsservice/dao/entities/Transaction.java
@@ -0,0 +1,41 @@
+package hps.ma.transactionsservice.dao.entities;
+
+
+import hps.ma.transactionsservice.dao.enums.TransactionCategory;
+
+
+import hps.ma.transactionsservice.dao.enums.TransactionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor@NoArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    private String merchant;
+
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionCategory category;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    private String type;
+
+    @ManyToOne
+    private Card card;
+}
---
+++ microservices/transactions-service/src/main/java/hps/ma/transactionsservice/dao/enums/TransactionCategory.java
@@ -0,0 +1,9 @@
+package hps.ma.transactionsservice.dao.enums;
+
+public enum TransactionCategory {
+
+    FOOD, TRANSPORT, ENTERTAINMENT, ONLINE, UTILITIES, OTHER
+
+}
+
+
