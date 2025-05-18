a05bde3c9ac15385bd3cfe0eb1574feb1655c2d2
Samy <Rhalimsami8@gmail.com>
2025-05-18 21:31:37 +0100

    coomit for updating the logic of biometric auth

--- frontend/android/app/src/main/kotlin/com/example/sami/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/sami/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.sami
+package com.example.sami  // ✅ Update this if your package name is different
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class   MainActivity: FlutterFragmentActivity()  // ✅ FIXED
