5391a81f371b173bc2ebd8d313e9ee6df8295964
Samy <Rhalimsami8@gmail.com>
2025-05-21 16:13:22 +0100

    commit for enabling the kafka @EnableKafka in service Event(notification)

--- microservices/event-service/src/main/java/hps/ma/eventservice/EventServiceApplication.java
+++ microservices/event-service/src/main/java/hps/ma/eventservice/EventServiceApplication.java
@@ -2,8 +2,10 @@ package hps.ma.eventservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class EventServiceApplication {
 
     public static void main(String[] args) {
