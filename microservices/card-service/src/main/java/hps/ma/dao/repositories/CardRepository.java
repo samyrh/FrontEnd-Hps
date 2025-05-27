a1c2145e8896df48de7e5da92b208775baa4dffe
Samy <Rhalimsami8@gmail.com>
2025-05-27 17:00:21 +0100

    commit for add logic of controller (get all physical cards by cardholderID) + creating repository + add a jwt cle for extracting the token

--- microservices/card-service/src/main/java/hps/ma/dao/repositories/CardRepository.java
+++ microservices/card-service/src/main/java/hps/ma/dao/repositories/CardRepository.java
@@ -2,6 +2,7 @@ package hps.ma.dao.repositories;
 
 
 import hps.ma.dao.entities.Card;
+import hps.ma.dao.enums.CardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +13,14 @@ public interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findByCardholderId(Long cardholderId);
 
-    List<Card> findByAgentId(Long agentId);
+    List<Card> findByManagerId(Long managerId);
+
 
     List<Card> findByCardPackId(Long cardPackId);
 
     List<Card> findByStatus(String status);
+
+    List<Card> findByCardholderIdAndType(Long cardholderId, CardType type);
+
+
 }
