72666c065cc41b92b8d9355149f1b1d1ae37f409
Samy <Rhalimsami8@gmail.com>
2025-06-22 04:32:16 +0100

    add layer feign client in event service

---
+++ microservices/event-service/src/main/java/hps/ma/eventservice/feign_client/CardholderService.java
@@ -0,0 +1,17 @@
+package hps.ma.eventservice.feign_client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+@FeignClient(name = "user-service", url = "http://localhost:9999/api/cardholders")
+public interface CardholderService {
+
+    @GetMapping("/internal/by-id/{id}")
+    Map<String, Object> getCardholderById(@PathVariable("id") Long id);
+
+    @GetMapping("/internal/by-username/{username}")
+    Map<String, Object> getCardholderIdByUsername(@PathVariable("username") String username);
+}
