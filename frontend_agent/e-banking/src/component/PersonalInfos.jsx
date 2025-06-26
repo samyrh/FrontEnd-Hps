45fe32995e1d4c9f2590b9e2c558d11bdb2f83a2
NADA <nadasenhaji@gmail.com>
2025-06-26 09:40:48 +0100

    commit for personalinfos/agent

--- frontend_agent/e-banking/src/component/PersonalInfos.jsx
+++ frontend_agent/e-banking/src/component/PersonalInfos.jsx
@@ -1,43 +1,59 @@
 // src/component/PersonalInfos.jsx
 import {
-    Box, Flex, FormControl, FormLabel, Input, Textarea, Text, Avatar, Button, Icon,
-    useColorModeValue
+    Box,
+    Flex,
+    FormControl,
+    FormLabel,
+    Input,
+    Text,
+    Button,
+    Icon,
+    useColorModeValue,
 } from '@chakra-ui/react';
-import { FaUser, FaEnvelope, FaImage, FaEdit } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa';
+import { useState } from 'react';
 
 export default function PersonalInfos() {
+    const [generatedPassword, setGeneratedPassword] = useState('');
+
+    const generatePassword = () => {
+        const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+        const password = Array.from({ length: 12 }, () =>
+            charset[Math.floor(Math.random() * charset.length)]
+        ).join('');
+        setGeneratedPassword(password);
+    };
+
     return (
         <Box borderRadius="20px" p="6" bg={useColorModeValue('white', '#141f52')}>
-            <Text fontSize="lg" fontWeight="bold" mb="2">Personal information</Text>
-            <Text fontSize="sm" color="gray.400" mb="6">Lorem ipsum dolor sit amet consectetur adipiscing.</Text>
+            <Text fontSize="lg" fontWeight="bold" mb="2">Personal Information</Text>
+            <Text fontSize="sm" color="gray.400" mb="6">Please enter the user's details.</Text>
 
             <Flex direction="column" gap="24px">
                 <FormControl>
                     <FormLabel><Icon as={FaUser} mr="2" />Full name</FormLabel>
-                    <Input defaultValue="John Carter" />
+                    <Input placeholder="Enter full name" />
                 </FormControl>
 
                 <FormControl>
                     <FormLabel><Icon as={FaEnvelope} mr="2" />Email address</FormLabel>
-                    <Input defaultValue="john@dashdark.com" />
+                    <Input placeholder="Enter email" />
                 </FormControl>
 
                 <FormControl>
-                    <FormLabel><Icon as={FaImage} mr="2" />Photo</FormLabel>
-                    <Flex gap="20px" align="center">
-                        <Avatar name="John Carter" src="https://i.pravatar.cc/150?img=12" />
-                        <Button size="sm" variant="ghost" colorScheme="red">Delete</Button>
-                        <Box textAlign="center">
-                            <Text color="purple.400" fontSize="sm" cursor="pointer">Click to upload</Text>
-                            <Text fontSize="xs" color="gray.500">SVG, PNG, JPG or GIF (max. 800x400px)</Text>
-                        </Box>
+                    <FormLabel><Icon as={FaLock} mr="2" />Password</FormLabel>
+                    <Flex gap="12px">
+                        <Input value={generatedPassword} readOnly placeholder="Click to generate a password" />
+                        <Button colorScheme="purple" onClick={generatePassword}>Generate</Button>
                     </Flex>
                 </FormControl>
 
-                <FormControl>
-                    <FormLabel><Icon as={FaEdit} mr="2" />Short description</FormLabel>
-                    <Textarea placeholder="Write a short bio about you..." />
-                </FormControl>
+                {/* Add User Button */}
+                <Flex justify="center" mt="20px">
+                    <Button colorScheme="purple" size="lg" px="10">
+                        Add User
+                    </Button>
+                </Flex>
             </Flex>
         </Box>
     );
