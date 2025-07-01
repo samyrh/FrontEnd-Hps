e88a0ce0822462cd2210d85836398a58a0476ab9
Samy <Rhalimsami8@gmail.com>
2025-07-01 13:43:54 +0100

    Restore project to state of commit 413a88e

--- frontend_agent/e-banking/src/Routes/Routes.jsx
+++ frontend_agent/e-banking/src/Routes/Routes.jsx
@@ -6,21 +6,18 @@ import UsersDetails from '../layers/UsersDetails';
 import MainDashboard from '../layers/MainDashboard';
 import TravelPlan from '../layers/TravelPlan';
 import AgentProfile from '../layers/AgentProfile';
-import CardsDetails from '../layers/CardsDetails';
-import Notifications from "../layers/Notifications";
-
+import CardsDetails from '../layers/CardsDetails'; // ✅ Add this
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<MainDashboard />} />
             <Route path="/cards" element={<Cards />} />
-            <Route path="/card-details" element={<CardsDetails />} />
+            <Route path="/card-details" element={<CardsDetails />} /> {/* ✅ New route */}
             <Route path="/users" element={<Users />} />
             <Route path="/users/details" element={<UsersDetails />} />
             <Route path="/travel" element={<TravelPlan />} />
             <Route path="/profile" element={<AgentProfile />} />
-            <Route path="/notifications" element={<Notifications />} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
